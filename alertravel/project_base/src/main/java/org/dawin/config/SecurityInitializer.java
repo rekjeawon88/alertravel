3a042d62d845664592d9b82030fb7918ebb8e9b3
Kim Jaewon <rekjeawon88@naver.com>
2023-11-16 13:32:07 +0900

    폴더 경로 변경

--- alertravel/project_base/src/main/java/org/dawin/config/SecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package org.dawin.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
